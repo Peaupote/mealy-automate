4fb79e23dbcb8e46de738afcdce4e55d48222800
Peaupote <maxime.flin@gmail.com>
2019-04-12 13:24:46 +0200

    fix generator.c

--- generator.c
+++ generator.c
@@ -4,10 +4,22 @@
 int nb_states, nb_letters, size;
 u_int32_t sup, count;
 u_int8_t *delta, *rho;
+char debug = 0;
 
-void pb(u_int32_t t) {
+void pb(u_int32_t t, int indent) {
+    for (int i = 0; i <= indent; i++) printf(" ");
     for (unsigned int i = 0; i < size; i++) {
         printf("%d", (t >> i) & 1);
+    }
+    printf("\n");
+}
+
+void pt(u_int8_t *tab) {
+    unsigned int p, x;
+    for (p = 0; p < nb_states; p++) {
+        for (x = 0; x < nb_letters; x++) {
+            printf("%u ", tab[p * nb_letters + x]);
+        }
     }
     printf("\n");
 }
@@ -15,12 +27,12 @@ void pb(u_int32_t t) {
 int valid_rho() {
     unsigned int p, x, index, out;
     for (p = 0; p < nb_states; p++) {
-        out = 1UL << nb_letters;
+        out = 0;
         for(x = 0; x < nb_letters; x++) {
             index = p * nb_letters + x;
             if (rho[index] == 0xFF) continue;
-            if (((1 << rho[index]) & out) != 0) return 0;
-            out ^= 1 << rho[index];
+            if ((1UL << rho[index]) & out) return 0;
+            out ^= 1UL << rho[index];
         }
     }
 
@@ -30,12 +42,12 @@ int valid_rho() {
 int valid_delta() {
     unsigned int p, x, index, out;
     for(x = 0; x < nb_letters; x++) {
-        out = 1UL << nb_states;
+        out = 0;
         for(p = 0; p < nb_states; p++) {
             index = p * nb_letters + x;
             if (delta[index] == 0xFF) continue;
-            if (((1 << delta[index]) & out) != 0) return 0;
-            out ^= 1 << delta[index];
+            if ((1UL << delta[index]) & out) return 0;
+            out ^= 1UL << delta[index];
         }
     }
 
@@ -43,10 +55,8 @@ int valid_delta() {
 }
 
 unsigned int pop(u_int32_t *tab, unsigned int i) {
-    //printf("pop %u\n", i);
-    //pb(*tab);
     for (; i < size; i++) {
-        if (((*tab) & (1UL << i)) == 0) {
+        if ((((*tab) >> i) & 1) == 0) {
             *tab ^= 1UL << i;
             return i;
         }
@@ -56,36 +66,34 @@ unsigned int pop(u_int32_t *tab, unsigned int i) {
 }
 
 unsigned int iter(u_int32_t *tab, unsigned int i) {
-    //printf("iter %u\n", i);
-    //pb(*tab);
-    if (i) (*tab) ^= 1UL << i++;
+    if (i>=0) (*tab) ^= 1UL << i++;
+    if (i<0) i = 0;
     while (i < size && ((*tab) >> i) & 1) i++;
     if (i < size) (*tab) ^= 1UL << i;
     return i;
 }
 
-unsigned int len(u_int32_t tab) {
-    unsigned int i, c = 0;
-    for (i = 0; i < size; i++) {
-        if ((tab & (1UL << i)) == 0) c++;
-    }
-    return c;
-}
-
 void rec(u_int8_t start_p, u_int8_t start_x,
          u_int8_t prev_p, u_int8_t prev_x,
-         u_int32_t sources, u_int32_t targets) {
-    //printf("====\n");
-    //pb(sources);
-    //pb(targets);
+         u_int32_t sources, u_int32_t targets, int ident) {
+    if (debug) {
+        printf("====\n");
+        pb(sources, ident);
+        pb(targets, ident);
+    }
+
     if (sup - 1 == sources && sup - 1 == targets) {
         count++;
+        if (debug) {
+            pt(delta);
+            pt(rho);
+        }
         printf("find one %u\n", count);
         return;
     }
 
     unsigned char add = 0;
-    unsigned int index, i = 0;
+    unsigned int index, i = -1;
     u_int8_t next_p, next_x;
 
     if (prev_x == 0xFF) {
@@ -93,34 +101,36 @@ void rec(u_int8_t start_p, u_int8_t start_x,
         index = pop(&sources, 0);
         start_p = index / nb_letters;
         start_x = index % nb_letters;
-        //printf("new start %u, %u\n", start_p, start_x);
+        prev_p = start_p;
+        prev_x = start_x;
+        if (debug)
+            printf("%*s new start %u, %u\n", ident, "", start_p, start_x);
     }
 
-    //u_int32_t tmp = targets;
     while((i = iter(&targets, i)) < size) {
         next_p = i / nb_letters;
         next_x = i % nb_letters;
-        //printf("in while %u\n", i);
-        //pb(tmp);
-        //pb(targets);
-        //printf("next %u, %u\n", next_p, next_x);
+        if (debug) {
+            printf("%*s in while %u\n", ident, "", i);
+            pb(sources, ident);
+            pb(targets, ident);
+            printf("%*s next %u, %u\n", ident, "", next_p, next_x);
+        }
 
         index = prev_p * nb_letters + prev_x;
         delta[index] = next_p;
         rho[index] = next_x;
 
         if (next_p != start_p || next_x != start_x) {
-            //printf("not loop\n");
             if (valid_delta(delta) && valid_rho(rho)) {
                 index = 1UL << (next_p * nb_letters + next_x);
                 sources ^= index;
-                rec(start_p, start_x, next_p, next_x, sources, targets);
+                rec(start_p, start_x, next_p, next_x, sources, targets, ident + 4);
                 sources ^= index;
             }
         } else {
-            //printf("loop\n");
             if (valid_delta(delta) && valid_rho(rho)) {
-                rec(0xFF, 0xFF, 0xFF, 0xFF, sources, targets);
+                rec(0xFF, 0xFF, 0xFF, 0xFF, sources, targets, ident + 4);
             }
         }
 
@@ -133,7 +143,8 @@ void rec(u_int8_t start_p, u_int8_t start_x,
         sources ^= 1 << (start_p * nb_letters + start_x);
     }
 
-    //printf("end\n\n");
+    if (debug)
+        printf("%*s end\n", ident, "");
 }
 
 int main (int argc, char *argv[]) {
@@ -144,6 +155,8 @@ int main (int argc, char *argv[]) {
 
     nb_states = atoi(argv[1]);
     nb_letters = atoi(argv[2]);
+
+    debug = (argc == 4);
 
     size = nb_letters * nb_states;
     sup = (1UL << size);
@@ -163,7 +176,7 @@ int main (int argc, char *argv[]) {
 
     printf("alloc done.\n");
 
-    rec(0xFF, 0xFF, 0xFF, 0xFF, 0, 0);
+    rec(0xFF, 0xFF, 0xFF, 0xFF, 0, 0, 0);
 
     printf("%u\n", count);
 }
